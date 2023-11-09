0312ab0895786b1a6c9b7d708f9ad5b12d5ce51d
petr lukas <petr.symeon.lukas@gmail.com>
2023-11-09 12:00:39 +0100

    http client commented out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClient
+  //  HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
